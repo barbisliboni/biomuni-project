4f0a2febf8f38b19591bd247d43933973eaebbc6
barbisliboni <niinaliboni@gmail.com>
2022-04-07 21:48:07 -0300

    adding drone and sprinkler repository package and interface

---
+++ biomuni/src/main/java/br/com/project/biomuni/repository/DroneRepository.java
@@ -0,0 +1,9 @@
+package br.com.project.biomuni.repository;
+
+import br.com.project.biomuni.model.Drone;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DroneRepository extends JpaRepository<Drone, Long> {
+}
